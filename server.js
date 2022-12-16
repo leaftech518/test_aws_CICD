f776639d52a5190af1e58bfe4cd1aec7b69ee14e
leaftech518 <80030510+leaftech518@users.noreply.github.com>
2022-12-16 17:31:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const DATABASE_URL = " mongodb+srv://pgroom:pgroom@cluster0.l4lhcji.mongodb.net/?retryWrites=true&w=majority"
+const DATABASE_URL = "mongodb+srv://pgroom:pgroom@cluster0.l4lhcji.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
