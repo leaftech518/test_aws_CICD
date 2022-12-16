3cf07435625c488aa2e9645124cc1fde807425e7
codeninjakun <akshaysinghxyz@gmail.com>
2022-12-16 22:08:21 +0530

    with morgan

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 // const DATABASE_URL = " mongodb+srv://pgroom:pgroom@cluster0.l4lhcji.mongodb.net/?retryWrites=true&w=majority"
+const morgan = require("morgan")
+
+app.use(morgan("tiny"));
+
 
 mongoose.connect("mongodb+srv://pgroom:pgroom@cluster0.l4lhcji.mongodb.net/testing", { useNewUrlParser: true })
 const db = mongoose.connection
